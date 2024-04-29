977d184bc4158986d9caf9e1efed09e1bbe2e950
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-04-29 10:50:44 +0600

    basic structure and styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@
 const App = () => {
   return (
     <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Mihal</p>
+        </nav>
+      </section>
+      <section></section>
     </div>
   );
 }
