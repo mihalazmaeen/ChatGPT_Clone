f626632678460bdc2cf69cd4ef0346b8e90ebae8
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-04-29 12:08:56 +0600

    overall layout design

--- src/App.js
+++ src/App.js
@@ -5,12 +5,26 @@ const App = () => {
     <div className="app">
       <section className="side-bar">
         <button>+ New Chat</button>
-        <ul className="history"></ul>
+        <ul className="history">
+          <li>New Chat</li>
+        </ul>
         <nav>
           <p>Made by Mihal</p>
         </nav>
       </section>
-      <section></section>
+      <section className="main">
+        <h1> VangariGPT</h1>
+        <ul className="feed"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit">⇒</div>
+          </div>
+          <p>
+            VangariGPT is a clone version of ChatGPT. Version "Almost Vanga".
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
