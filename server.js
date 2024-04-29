142ac5359022c40c4ab6816537009aab9b0155d4
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-04-29 12:33:31 +0600

    server js configure

---
+++ server.js
@@ -0,0 +1,7 @@
+const PORT=8000
+const express= require('express')
+const cors= require('cors')
+const app= express()
+app.use(express.json())
+app.use(cors())
+app.listen(PORT,()=>console.log('Server is running'+ PORT))
